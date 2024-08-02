9300612aa6beeeb1280919f16763e759f8bea274
vincent <vinnymummo@gmail.com>
2024-08-02 09:48:52 +0300

    testing ci/cd

--- server/config.py
+++ server/config.py
@@ -4,4 +4,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///Ireporter.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CLOUDINARY_URL = os.getenv('CLOUDINARY_URL')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'IeL3lkZx1jBVMW6VdYrdbNAM2uc')
+    SECRET_KEY = os.getenv('SECRET_KEY')
